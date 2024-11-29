3871876e3aba1b53aab05a759cf093239a90d2c7
Vamshk0603 <1rn21cs178.vamshikrishnaa@rnsit.ac.in>
2024-11-29 14:45:26 +0530

    serializers-api

---
+++ backend/APIs/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Resume, UserSkill, ResumeSkill, Questionnaire, Feedback
+
+
+class ResumeSerializer(serializers.ModelSerializer):
+    skills = serializers.StringRelatedField(many=True, read_only=True)  # Show skill names
+    user = serializers.StringRelatedField()  # Show username instead 
+
+    class Meta:
+        model = Resume
+        fields = ['id', 'user', 'skills', 'certifications', 'projects', 'upload_date']
+
+
+class ResumeSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResumeSkill
+        fields = ['id', 'name']
+
+
+class UserSkillSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()  
+    skill = ResumeSkillSerializer()  
+
+    class Meta:
+        model = UserSkill
+        fields = ['id', 'user', 'skill']
+
+
+class QuestionnaireSerializer(serializers.ModelSerializer):
+    skill = serializers.StringRelatedField()  
+
+    class Meta:
+        model = Questionnaire
+        fields = ['id', 'skill', 'question', 'complexity']
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()  # Show username
+    session_id = serializers.UUIDField(read_only=True)  #session_id ss immutable
+
+    class Meta:
+        model = Feedback
+        fields = ['id', 'user', 'datetime', 'feedback_content', 'session_id']
